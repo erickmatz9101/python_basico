5591f3f14b52c85d256a71637b1144322b71ed86
Erick Martinez <erick_martinez9101@outlook.com>
2022-01-26 20:28:28 -0600

    En esta segúnda modificacion hice uso de las funciones para modularizar el código y no repetir tantas lineas por medio de la funcion conversor

--- conversor.py
+++ conversor.py
@@ -1,6 +1,23 @@
 
 from ast import If
 
+##Función que permite hacer la conversion de pesos a dólares según el país 
+
+def conversor(tipo_pesos, valor_dolar):
+
+    pesos = input("¿Cuántos pesos " + tipo_pesos + " tienes? ") #variable que guarda la cantidad de pesos a convertir
+
+    pesos = float(pesos) #transformando a número decimal
+
+    dolares = pesos/valor_dolar #variable que convierte de pesos a dolares
+
+    dolares = round(dolares,2) #redondeando a 2 decimales la conversión
+
+    dolares =str(dolares) #variable que convierte la cantidad de dolares en texto
+
+    print("Tienes $ " + dolares + " dólares")
+
+
 
 menu = """
 Bienvenido al conversor de monedas 💵💵🤑
@@ -11,92 +28,24 @@ Bienvenido al conversor de monedas 💵💵🤑
 
 3.- Pesos Mexicanos
 
-Elige una opción: """
+Elige una opción:  """
 
 
 opcion = int(input(menu))
 
 if opcion == 1:
 
-    pesos = input("¿Cuántos pesos Colombianos tienes?: ") #variable que guarda la cantidad de pesos colombianos a convertir
-
-    #haciendo la transformación del valor al número decimal (casteo)
-
-    pesos = float(pesos)
-
-    #Variable que gusrada el valor del dolar en pesos colombianos 
-
-    valor_dolar = 3875
-
-    #Creando la variable para convertir de pesos a dolares
-
-    dolares= pesos / valor_dolar
-
-    #Para rendondear a 2 decimales la converión
-
-    dolares = round(dolares,2)
-
-    #Variable que convierte la cantidad de dolares en texto (casteo)
-
-    dolares = str(dolares)
-
-    print("Tienes $" + dolares + " dólares")
+    conversor("Colombianos", 3875)
 
 
 elif opcion ==2:
 
-    pesos = input("¿Cuántos pesos Argentinos tienes?: ") #variable que guarda la cantidad de pesos colombianos a convertir
-
-    #haciendo la transformación del valor al número decimal (casteo)
-
-    pesos = float(pesos)
-
-    #Variable que gusrada el valor del dolar en pesos colombianos 
-
-    valor_dolar = 65
-
-    #Creando la variable para convertir de pesos a dolares
-
-    dolares= pesos / valor_dolar
-
-    #Para rendondear a 2 decimales la converión
-
-    dolares = round(dolares,2)
-
-    #Variable que convierte la cantidad de dolares en texto (casteo)
-
-    dolares = str(dolares)
-
-    print("Tienes $" + dolares + " dólares")
-
+    conversor("Argentinos", 65)
     
 
 elif opcion == 3:
 
-    pesos = input("¿Cuántos pesos Mexicanos tienes?: ") #variable que guarda la cantidad de pesos colombianos a convertir
-
-    #haciendo la transformación del valor al número decimal (casteo)
-
-    pesos = float(pesos)
-
-    #Variable que gusrada el valor del dolar en pesos colombianos 
-
-    valor_dolar = 22
-
-    #Creando la variable para convertir de pesos a dolares
-
-    dolares= pesos / valor_dolar
-
-    #Para rendondear a 2 decimales la converión
-
-    dolares = round(dolares,2)
-
-    #Variable que convierte la cantidad de dolares en texto (casteo)
-
-    dolares = str(dolares)
-
-    print("Tienes $" + dolares + " dólares")
-
+    conversor("Mexicanos", 24)
 else:
 
     print("Elegiste un opción que no es válida")
@@ -114,4 +63,11 @@ else:
 
 """La  función round en Python es la que me permite redondear una cantidad al número de dígitos que yo le indique y devuelve un número
 de punto flotante round(cantidad o variable, numero de dígitos despues del punto), en mi ejemplo quedó de la siguiente manera:
-round(dolares, 2) lo cualquiere decir que después del punto decimal va a imprimir sólo 2 digitos"""
+round(dolares, 2) lo cualquiere decir que después del punto decimal va a imprimir sólo 2 digitos
+
+En ésta segunda modificación se usaron las funciones las cuales permiten que se pueda modularizar el código para no tener que escribir
+demasiada líneas, para este caso se utilizó la función conversor a la cual se la pasó como parámetro el tipo de peso (colombianos,
+argentinos, mexicanos) y el valor del dolar en este caso segun la moneda de cambio es el costo que tiene por dolar por tal 
+motivo me convenia poner esos datos como parámetros de mi funcion y asi reutilizar mi código.
+
+Por buenas prácticas las funciones deben colocarse en la parte superior de el código"""
