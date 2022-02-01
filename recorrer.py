ff8209f6a02cf884f85fa469a6064c16058a350e
Erick Martinez <erick_martinez9101@outlook.com>
2022-01-31 18:45:18 -0600

    En ésta leccion se muestra como recorrer cadenas de caracyeres utilizando el ciclo for

---
+++ recorrer.py
@@ -0,0 +1,29 @@
+# nombre = input("Escribe tu nombre: ")
+
+    #for letra in nombre:
+     #   print(letra)
+
+
+
+
+
+   
+
+##Para recorrer una frase e imprimir caracter por caracter pero que lo devuelva en mayúscula, sería de la siguiente manera:
+
+def run():
+
+    frase = input("Escribe la frase: ")
+
+    for caracter in frase:
+        print(caracter.upper())
+
+
+if __name__  == '__main__':
+    run()
+
+
+
+"""Éste programa gebera un ciclo que recorre caracter por caracter hasta el final.
+la variable LETRA representa a las repeticiones en cada vuelta del ciclo for
+Ciclo For: representa que para las letras en el nombre vamos a imprimir cada letra en cada vuelta del ciclo """    
